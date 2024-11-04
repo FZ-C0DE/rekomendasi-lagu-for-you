998ffed9aa5a7e0be651040341900cad48493499
FZ-C0DE <fatazikrillah222@gmail.com>
2024-11-04 17:52:43 +0700

    Add environment variables and update .gitignore

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
+
 const express = require("express");
 const axios = require("axios");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-// Client ID dan Secret dari Spotify API
-const client_id = "98e14b7394344007be6b1c8e067a2f9d";
-const client_secret = "6e61596b01774725a12b8d5d468a6064";
+// Client ID dan Secret dari file .env
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
 
-// Middleware untuk melayani file statis dari folder "public"
-app.use(express.static("public"));
+// Middleware untuk melayani file statis dari root folder
+app.use(express.static("."));
 
 // Endpoint untuk mendapatkan token akses dari Spotify API
 app.get("/get-token", async (req, res) => {
@@ -17,7 +20,7 @@ app.get("/get-token", async (req, res) => {
     url: "https://accounts.spotify.com/api/token",
     headers: {
       Authorization:
-        "Basic " + Buffer.from(client_id + ":" + client_secret).toString("base64"),
+        "Basic " + Buffer.from(clientId + ":" + clientSecret).toString("base64"),
       "Content-Type": "application/x-www-form-urlencoded",
     },
     data: "grant_type=client_credentials",
@@ -27,8 +30,14 @@ app.get("/get-token", async (req, res) => {
     const response = await axios(authOptions);
     res.json({ access_token: response.data.access_token });
   } catch (error) {
+    console.error("Error:", error); // Log error untuk debugging
     res.status(500).json({ error: "Gagal mendapatkan token akses" });
   }
+});
+
+// Route utama untuk melayani file index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Jalankan server di port 3000
