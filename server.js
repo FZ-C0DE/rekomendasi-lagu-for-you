94cbe95df794f9f454f087523042aee19728a8d9
FZ-C0DE <fatazikrillah222@gmail.com>
2024-11-04 19:27:06 +0700

    first commit

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 require('dotenv').config();
-
-const express = require("express");
 const axios = require("axios");
-const path = require("path");
-const app = express();
-const port = 3000;
 
-// Client ID dan Secret dari file .env
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
+module.exports = async (req, res) => {
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
 
-// Middleware untuk melayani file statis dari root folder
-app.use(express.static("."));
-
-// Endpoint untuk mendapatkan token akses dari Spotify API
-app.get("/get-token", async (req, res) => {
   const authOptions = {
     method: "post",
     url: "https://accounts.spotify.com/api/token",
@@ -30,17 +20,7 @@ app.get("/get-token", async (req, res) => {
     const response = await axios(authOptions);
     res.json({ access_token: response.data.access_token });
   } catch (error) {
-    console.error("Error:", error); // Log error untuk debugging
+    console.error("Error:", error);
     res.status(500).json({ error: "Gagal mendapatkan token akses" });
   }
-});
-
-// Route utama untuk melayani file index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Jalankan server di port 3000
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
+};
